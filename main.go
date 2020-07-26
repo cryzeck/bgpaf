bdd528b62becf0664b9bc8fbd49a60d6d1acc06b
Jan Astrup <jesastrup@gmail.com>
2020-07-26 23:35:21 +0200

    gofmtftw.

--- main.go
+++ main.go
@@ -1,117 +1,117 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "bufio"
-  "encoding/json"
-  "net/url"
-  "net/http"
-  "strings"
-  "gopkg.in/yaml.v2"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 type Config struct {
-  Host    string  `yaml:"host"`
-  Port    string  `yaml:"port"`
-	Path    string  `yaml:"path"`
-	ApiKey  string  `yaml:"apikey"`
+	Host   string `yaml:"host"`
+	Port   string `yaml:"port"`
+	Path   string `yaml:"path"`
+	ApiKey string `yaml:"apikey"`
 }
 
 func ReadConfig(configPath string) (*Config, error) {
-  config := &Config{}
-  file, err := os.Open(configPath)
-  if err != nil {
-    return nil, err
-  }
+	config := &Config{}
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
 
-  defer file.Close()
-  d := yaml.NewDecoder(file)
+	defer file.Close()
+	d := yaml.NewDecoder(file)
 
-  if err := d.Decode(&config); err != nil {
-    return nil, err
-  }
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
 
-  return config, nil
+	return config, nil
 }
 
 type policy struct {
-  Op      string
-  Path    []string
-  Value   string
+	Op    string
+	Path  []string
+	Value string
 }
 
 func CreatePolicies(input []string) []*policy {
-  policies := []*policy{}
-  del := new(policy)
-  del.Op = "delete"
-  del.Path = []string{"policy", "prefix-list"}
-  del.Value = "test-list"
-  policies = append(policies, del)
+	policies := []*policy{}
+	del := new(policy)
+	del.Op = "delete"
+	del.Path = []string{"policy", "prefix-list"}
+	del.Value = "test-list"
+	policies = append(policies, del)
 
-  for i := 0; i < len(input); i++ {
-    pol := new(policy)
-    per := new(policy)
-    pol.Op = "set"
-    pol.Path = []string{"policy", "prefix-list", "test-list", "rule", fmt.Sprintf("%d", i+1), "action"}
-    pol.Value = "permit"
-    policies = append(policies, pol)
-    per.Op = "set"
-    per.Path = []string{"policy", "prefix-list", "test-list", "rule", fmt.Sprintf("%d", i+1), "prefix"}
-    per.Value = input[i]
-    policies = append(policies, per)
-  }
-  return policies
+	for i := 0; i < len(input); i++ {
+		pol := new(policy)
+		per := new(policy)
+		pol.Op = "set"
+		pol.Path = []string{"policy", "prefix-list", "test-list", "rule", fmt.Sprintf("%d", i+1), "action"}
+		pol.Value = "permit"
+		policies = append(policies, pol)
+		per.Op = "set"
+		per.Path = []string{"policy", "prefix-list", "test-list", "rule", fmt.Sprintf("%d", i+1), "prefix"}
+		per.Value = input[i]
+		policies = append(policies, per)
+	}
+	return policies
 }
 
-func ReadInput() []string{
-  var lines []string
-  scanner := bufio.NewScanner(os.Stdin)
+func ReadInput() []string {
+	var lines []string
+	scanner := bufio.NewScanner(os.Stdin)
 
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
-  if err := scanner.Err(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-  }
-  return lines
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	return lines
 }
 
 func main() {
-  cfg, err := ReadConfig("./config.yaml")
-  if err != nil {
-    log.Fatal(err)
-  }
+	cfg, err := ReadConfig("./config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  lines := ReadInput()
-  e, err := json.Marshal(CreatePolicies(lines))
-  if err != nil {
-    log.Fatal(err)
-  }
+	lines := ReadInput()
+	e, err := json.Marshal(CreatePolicies(lines))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  myurl := url.URL {
-    Scheme: "https",
-    Host: cfg.Host+":"+cfg.Port,
-    Path: cfg.Path,
-  }
+	myurl := url.URL{
+		Scheme: "https",
+		Host:   cfg.Host + ":" + cfg.Port,
+		Path:   cfg.Path,
+	}
 
-  data := url.Values{}
-  data.Set("data", strings.ToLower(string(e)))
-  data.Set("key", cfg.ApiKey)
+	data := url.Values{}
+	data.Set("data", strings.ToLower(string(e)))
+	data.Set("key", cfg.ApiKey)
 
-  fmt.Println(myurl.String())
+	fmt.Println(myurl.String())
 
-  resp, err := http.PostForm(myurl.String(), data)
+	resp, err := http.PostForm(myurl.String(), data)
 
-  fmt.Println(data)
+	fmt.Println(data)
 
-  if err != nil {
-    log.Fatal(err)
-  }
-  if resp == nil {
-    fmt.Println("No response?")
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp == nil {
+		fmt.Println("No response?")
+	}
 
-  fmt.Println(resp)
+	fmt.Println(resp)
 }
