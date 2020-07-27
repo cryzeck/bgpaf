55ad044e46696c630c01f772c303907e544950c8
Jan Astrup <jesastrup@gmail.com>
2020-07-27 22:50:07 +0200

    Working version

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/cryzeck/irrdb"
+	"gopkg.in/yaml.v2"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 	"strings"
-
-	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Host   string `yaml:"host"`
-	Port   string `yaml:"port"`
-	Path   string `yaml:"path"`
-	ApiKey string `yaml:"apikey"`
+	Irrdb  string   `yaml:"irrdb"`
+	Host   string   `yaml:"host"`
+	Port   string   `yaml:"port"`
+	ApiKey string   `yaml:"apikey"`
+	Peers  []string `yaml:"peers"`
 }
 
 func ReadConfig(configPath string) (*Config, error) {
@@ -34,7 +34,6 @@ func ReadConfig(configPath string) (*Config, error) {
 	if err := d.Decode(&config); err != nil {
 		return nil, err
 	}
-
 	return config, nil
 }
 
@@ -60,11 +59,11 @@ func (p *policy) CloneExtend(path ...string) *policy {
 	return &policy{p.Op, lis, p.Value}
 }
 
-func CreatePolicies(input []string) []*policy {
+func CreatePolicies(name string, input []string) []*policy {
 	policies := make([]*policy, 0, len(input)*2+1)
-	policies = append(policies, NewPolicy("delete", "policy", "prefix-list").SetValue("test-list"))
+	policies = append(policies, NewPolicy("delete", "policy", "prefix-list").SetValue(name))
 
-	rule := NewPolicy("set", "policy", "prefix-list", "test-list", "rule")
+	rule := NewPolicy("set", "policy", "prefix-list", name, "rule")
 	for i, value := range input {
 		key := strconv.Itoa(i + 1)
 
@@ -77,46 +76,51 @@ func CreatePolicies(input []string) []*policy {
 	return policies
 }
 
-func ReadInput() []string {
-	var lines []string
-	scanner := bufio.NewScanner(os.Stdin)
+func CreatePolicies6(name string, input []string) []*policy {
+	policies := make([]*policy, 0, len(input)*2+1)
+	policies = append(policies, NewPolicy("delete", "policy", "prefix-list").SetValue(name))
 
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	rule := NewPolicy("set", "policy", "prefix-list6", name, "rule")
+	for i, value := range input {
+		key := strconv.Itoa(i + 1)
+
+		policies = append(policies,
+			rule.CloneExtend(key, "action").SetValue("permit"),
+			rule.CloneExtend(key, "prefix").SetValue(value),
+		)
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	return lines
+
+	return policies
 }
 
-func main() {
-	cfg, err := ReadConfig("./config.yaml")
-	if err != nil {
-		log.Fatal(err)
+func UpdateFilter(name string, pref []string, host, port, key string) {
+	if strings.Contains(name, "v6") {
+		e, err := json.Marshal(CreatePolicies6(name, pref))
+		if err != nil {
+			log.Fatal(err)
+		}
+		Postfilter(string(e), host, port, key)
+	} else {
+		e, err := json.Marshal(CreatePolicies(name, pref))
+		if err != nil {
+			log.Fatal(err)
+		}
+		Postfilter(string(e), host, port, key)
 	}
+}
 
-	lines := ReadInput()
-	e, err := json.Marshal(CreatePolicies(lines))
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func Postfilter(e, host, port, key string) {
 	myurl := url.URL{
 		Scheme: "https",
-		Host:   cfg.Host + ":" + cfg.Port,
-		Path:   cfg.Path,
+		Host:   host + ":" + port,
+		Path:   "/configure",
 	}
 
 	data := url.Values{}
-	data.Set("data", strings.ToLower(string(e)))
-	data.Set("key", cfg.ApiKey)
-
-	fmt.Println(myurl.String())
+	data.Set("data", strings.ToLower(e))
+	data.Set("key", key)
 
 	resp, err := http.PostForm(myurl.String(), data)
-
-	fmt.Println(data)
 
 	if err != nil {
 		log.Fatal(err)
@@ -126,4 +130,34 @@ func main() {
 	}
 
 	fmt.Println(resp)
+}
+
+func main() {
+	Cfg, err := ReadConfig("./config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, peer := range Cfg.Peers {
+		fmt.Println("Requesting prefixes for " + peer)
+		resv4, err := irrdb.Query(Cfg.Irrdb, peer, "4")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			for _, pref := range resv4 {
+				fmt.Println("accept: " + pref)
+			}
+			UpdateFilter(peer+"-v4", resv4, Cfg.Host, Cfg.Port, Cfg.ApiKey)
+		}
+
+		fmt.Println("Building IPv6 filters:")
+		resv6, err := irrdb.Query(Cfg.Irrdb, peer, "6")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			for _, pref := range resv6 {
+				fmt.Println("accept: " + pref)
+			}
+			UpdateFilter(peer+"-v6", resv6, Cfg.Host, Cfg.Port, Cfg.ApiKey)
+		}
+	}
 }
